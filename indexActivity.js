005a5e211f20640d1bb18fd06e4651f7e1b32fe8
Marcus Fadrigalan <marcusfadrigalan@gmail.com>
2023-03-21 14:23:43 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!")
+
+let firstName = "Danielle"
+let lastName = "Castro"
+let age = 20
+let hobbies = ["Dancing", "Shopping", "Watching YouTube Videos", "Rollerskating", "Singing"];
+let homeAddress = {
+	houseNumber: "Block 6, Lot 26",
+	street: "Queen St",
+	subdivision: "Regency Executive Townhomes",
+	barangay: "Sampaloc 2",
+	city: "Dasmarinas",
+	province: "Cavite",
+	zipCode: 4114
+}
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
